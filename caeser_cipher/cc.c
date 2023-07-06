e4e8dfb431a085623aed0d8d6e51dcae3019e4d9
AparnaBaliga <124437620+AparnaBaliga@users.noreply.github.com>
2023-07-06 19:00:14 +0530

    Add files via upload

--- caeser_cipher/cc.c
+++ caeser_cipher/cc.c
@@ -5,13 +5,13 @@
 
 void enc(char* text, int sf) {
     for(int i = 0; text[i] != '\0'; ++i) {
-        if(islower(text[i])) {
-            text[i] = (text[i] - 'a' + sf) % 26 + 'a';
+        if(islower(text[i])) {  // To check if the character is a lowercase letter
+            text[i] = (text[i] - 'a' + sf) % 26 + 'a';  // Apply the encryption formula
         }
-        if(isupper(text[i])) {
+        if(isupper(text[i])) {  // To check if the character is an uppercase letter
             text[i] = (text[i] - 'A' + sf) % 26 + 'A';
         }
-        if(isdigit(text[i])) {
+        if(isdigit(text[i])) {  // To check if the character is a digit
             text[i] = (text[i] - '0' + sf) % 10 + '0';
         }
     }
@@ -20,7 +20,7 @@ void enc(char* text, int sf) {
 void dec(char* text, int sf) {
     for(int i = 0; text[i] != '\0'; ++i) {
         if(islower(text[i])) {
-            text[i] = (text[i] - 'a' - sf + 26) % 26 + 'a';
+            text[i] = (text[i] - 'a' - sf + 26) % 26 + 'a';  // Apply the decryption formula
         }
         if(isupper(text[i])) {
             text[i] = (text[i] - 'A' - sf + 26) % 26 + 'A';
@@ -33,20 +33,20 @@ void dec(char* text, int sf) {
 
 int main() {
    char text[200];
-   int sf, choice;  //shift factor
+   int sf, choice;  // shift factor
 
    printf("Enter 1 to decode and 2 to encode: ");
-   scanf("%d", &choice);
+   scanf("%d", &choice);  // Read the choice from the user (1 for decoding, 2 for encoding)
    printf("Enter the string: ");
-   scanf("%s", text);
+   scanf("%s", text);  // Read the string input from the user
    printf("Enter the shift-factor: ");
    scanf("%d", &sf);
 
    if(choice == 1)
-       dec(text, sf);
+       dec(text, sf);  // Call the dec function to decode the string
    else
-       enc(text, sf);
+       enc(text, sf);  // Call the enc function to encode the string
 
-   printf("The message is %s\n", text);
+   printf("The message is %s\n", text);  // Print the resulting message
    return 0;
 }
